4a82e6da259956065cf1fb1659fa0e373e3b05c4
James Karanja <james.karanja@zavora.co.ke>
2026-01-14 07:31:29 +0300

    feat: Implemented --help flag functionality

--- test-ralph-go/main_test.go
+++ test-ralph-go/main_test.go
@@ -58,3 +58,40 @@ func TestVersionFlag(t *testing.T) {
 		t.Errorf("Expected '%s\n', got '%s'", version, string(out))
 	}
 }
+
+func TestHelpFlag(t *testing.T) {
+	oldArgs := os.Args
+	oldStdout := os.Stdout
+	oldExit := exit
+
+	os.Args = []string{"cmd", "--help"}
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	exited := false
+	exit = func(code int) {
+		exited = true
+		panic("exit")
+	}
+
+	defer func() {
+		os.Stdout = oldStdout
+		os.Args = oldArgs
+		exit = oldExit
+		if r := recover(); r != nil && r != "exit" {
+			t.Fatalf("Unexpected panic: %v", r)
+		}
+	}()
+
+	main()
+
+	w.Close()
+	out, _ := io.ReadAll(r)
+
+	if !exited {
+		t.Error("Expected to exit, but did not")
+	}
+	if string(out) != "Usage: hello-world-cli [--version | --help]\n" {
+		t.Errorf("Expected 'Usage: hello-world-cli [--version | --help]\n', got '%s'", string(out))
+	}
+}
